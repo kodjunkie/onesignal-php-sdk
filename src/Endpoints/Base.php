a89d937388fa04250551e974ccb6c7a5d53dd37a
Lawrence Onah <paplow01@gmail.com>
2021-08-10 15:37:32 +0100

    :hammer_and_wrench: made app_id config optional

--- src/Endpoints/Base.php
+++ src/Endpoints/Base.php
@@ -2,6 +2,7 @@
 
 namespace Kodjunkie\OnesignalPhpSdk\Endpoints;
 
+use Kodjunkie\OnesignalPhpSdk\Exceptions\InvalidArgumentException;
 use Kodjunkie\OnesignalPhpSdk\Http\ClientInterface;
 
 abstract class Base
@@ -23,7 +24,11 @@ abstract class Base
     public function __construct(ClientInterface $client, array $config = [])
     {
         $this->client = $client;
-        $this->config = $config;
+        $this->config = [
+            'api_key' => trim($config['api_key']),
+            'auth_key' => trim($config['auth_key']),
+            'app_id' => trim($config['app_id']) ?? null,
+        ];
     }
 
     /**
@@ -32,5 +37,18 @@ abstract class Base
     protected function client(): ClientInterface
     {
         return $this->client->setAuthKey($this->config['api_key']);
+    }
+
+    /**
+     * @param string|null $appId
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    protected function getAppId(string $appId = null): string
+    {
+        if (is_null($appId) && is_null($this->config['app_id']))
+            throw new InvalidArgumentException('Missing required parameter (app_id).');
+
+        return is_null($appId) ? $this->config['app_id'] : $appId;
     }
 }
