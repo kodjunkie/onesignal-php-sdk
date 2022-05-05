b490f9d6b780be6ed9fd00ccc63bf530b2f75cf9
Lawrence Onah <paplow01@gmail.com>
2022-05-05 09:35:48 +0100

    Improved comment

--- config/onesignal.php
+++ config/onesignal.php
@@ -6,7 +6,7 @@ return [
     // Onesignal Auth key
     'auth_key' => env('ONESIGNAL_AUTH_KEY'),
     // Onesignal App ID (optional)
-    // Providing your app_id is only beneficial if you're working with a single OneSignal app
-    // and don't want to provide it all the time for endpoints that requires it.
+    // this is beneficial if you're working with a single OneSignal app
+    // so, you could pass "null" to methods that requires it.
     'app_id' => env('ONESIGNAL_APP_ID', null),
 ];
