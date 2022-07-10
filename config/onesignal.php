3042796c5f57ffe1778d41e60e4870713f5b45c6
Lawrence Onah <paplow01@gmail.com>
2022-07-10 09:50:18 +0100

    Minor clean-up

--- config/onesignal.php
+++ config/onesignal.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    // Onesignal API key
+    // Onesignal API Key
     'api_key' => env('ONESIGNAL_API_KEY'),
-    // Onesignal Auth key
+    // Onesignal Auth Key
     'auth_key' => env('ONESIGNAL_AUTH_KEY'),
     // Onesignal App ID (optional)
     // this is beneficial if you're working with a single OneSignal app
-    // so, you could pass "null" to methods that requires it.
-    'app_id' => env('ONESIGNAL_APP_ID', null),
+    // so, you could pass "null" to methods / functions that requires it.
+    'app_id' => env('ONESIGNAL_APP_ID'),
 ];
