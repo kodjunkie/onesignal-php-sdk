3dc4cc3a9d5bb1dbf8346586a89cec43cfae6095
Lawrence Onah <paplow01@gmail.com>
2022-08-11 12:58:18 +0100

    Updated tests

--- tests/Endpoints/NotificationTest.php
+++ tests/Endpoints/NotificationTest.php
@@ -48,6 +48,61 @@ class NotificationTest extends TestCase
         $this->assertTrue($success);
     }
 
+    public function test_it_can_get_a_notification()
+    {
+        $notificationId = 'notification-id';
+        $this->client->expects()->get('notifications/' . $notificationId, [
+            'app_id' => $this->config['app_id']
+        ])->once()->andReturn(true);
+
+        $success = (bool)$this->notification->get($notificationId);
+        $this->assertTrue($success);
+    }
+
+    public function test_it_can_create_a_notification()
+    {
+        $playerId = 'player-id';
+        $payload = [
+            'include_player_ids' => [$playerId],
+            'contents' => ['en' => 'Thank you for subscribing.'],
+            'headings' => ['en' => 'Subscription success'],
+            'data' => ['extra' => 'Some extra details']
+        ];
+        $this->client->expects()->post('notifications', array_merge([
+            'app_id' => $this->config['app_id']
+        ], $payload))->once()->andReturn(true);
+
+        $success = (bool)$this->notification->create($payload);
+        $this->assertTrue($success);
+    }
+
+    public function test_it_can_cancel_a_notification()
+    {
+        $notificationId = 'notification-id';
+        $this->client->expects()->delete('notifications/' . $notificationId, [
+            'app_id' => $this->config['app_id']
+        ])->once()->andReturn(true);
+
+        $success = (bool)$this->notification->cancel($notificationId);
+        $this->assertTrue($success);
+    }
+
+    public function test_it_can_get_a_notification_history()
+    {
+        $notificationId = 'notification-id';
+        $email = 'tester@gmail.ccom';
+        $event = Notification::SENT_EVENT;
+
+        $this->client->expects()->post('notifications/' . $notificationId . '/history', [
+            'app_id' => $this->config['app_id'],
+            'email' => $email,
+            'events' => $event
+        ])->once()->andReturn(true);
+
+        $success = (bool)$this->notification->history($notificationId, $email, $event);
+        $this->assertTrue($success);
+    }
+
     /**
      * This method is called after each test.
      */
